6826c03d03e575c0dab73bb3da11d0d19a4def39
Gregory Komar <xalvevs@gmail.com>
2024-12-03 23:16:41 +0200

    Bootstrap Ionic & Angular

---
+++ movie-browser-client-ionic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movie-browser-client-ionic',
+  webDir: 'www'
+};
+
+export default config;
